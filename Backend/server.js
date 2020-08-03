e1b972be00d4646474f9bc55f9fe46e26538fa9b
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2020-08-03 19:22:58 +0300

    deplyed heroku

--- Backend/server.js
+++ Backend/server.js
@@ -100,7 +100,7 @@ router.route("/delete/:id").delete((req, res) => {
     });
 });
 
-app.use(express.static(path.join(__dirname, "../build"));
+app.use(express.static(path.join(__dirname, "../build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
